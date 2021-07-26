aa49b04a6fce4e92309bcf5c1b9cd43776dbeacd
Rizky Putra Itaji <rizkyitaji@gmail.com>
2021-07-26 11:13:36 +0700

    Create search products at homepage

--- lib/ui/pages/user/product_page.dart
+++ lib/ui/pages/user/product_page.dart
@@ -1,6 +1,14 @@
 part of 'user.dart';
 
-class ProductPage extends StatelessWidget {
+class ProductPage extends StatefulWidget {
+  @override
+  _ProductPageState createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  final searchController = TextEditingController();
+  bool isNotEmpty = false;
+
   @override
   Widget build(BuildContext context) {
     var height = context.height / 2;
@@ -16,11 +24,53 @@ class ProductPage extends StatelessWidget {
           padding: EdgeInsets.symmetric(vertical: defMargin),
           child: Text('Products', style: poppins.copyWith(fontSize: 18)),
         ),
+        SizedBox(height: defMargin),
+        Container(
+          margin: EdgeInsets.symmetric(
+              horizontal: Screen.isMobile(context) ? defMargin : 100),
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: TextField(
+            controller: searchController,
+            decoration: InputDecoration(
+              hintText: 'Search',
+              contentPadding: EdgeInsets.fromLTRB(0, 10, 10, 10),
+              suffixIcon: InkWell(
+                onTap: () {
+                  searchController.text = '';
+                  setState(() => isNotEmpty = false);
+                  FocusScope.of(context).requestFocus(new FocusNode());
+                },
+                child: Icon(isNotEmpty ? Icons.close : Icons.search),
+              ),
+              suffixIconConstraints: BoxConstraints(),
+              border: InputBorder.none,
+            ),
+            onChanged: (value) {
+              if (value.isNotEmpty) {
+                isNotEmpty = true;
+              } else {
+                isNotEmpty = false;
+              }
+              setState(() {});
+            },
+          ),
+        ),
         Padding(
           padding: EdgeInsets.all(defMargin),
           child: StreamBuilder<QuerySnapshot>(
             stream: productRef.snapshots(),
             builder: (context, snapshot) {
+              var query = snapshot.data.docs.where((element) {
+                return element
+                    .data()['name']
+                    .toLowerCase()
+                    .contains(searchController.text.toLowerCase());
+              }).toList();
+
               if (snapshot.hasData) {
                 return GridView.count(
                   shrinkWrap: true,
@@ -29,7 +79,7 @@ class ProductPage extends StatelessWidget {
                   mainAxisSpacing: defMargin,
                   childAspectRatio:
                       Screen.large(context) ? 1.2 : width / height,
-                  children: snapshot.data.docs
+                  children: (isNotEmpty ? query : snapshot.data.docs)
                       .map((e) => InkWell(
                             onTap: () => Get.toNamed('/detail?detail=product',
                                 arguments: Product.fromSnapshot(e)),
